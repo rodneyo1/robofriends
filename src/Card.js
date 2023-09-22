8909761be792f0b6983c710472a12e5d1a70fb04
rod31 <rodneyotieno27@gmail.com>
2023-09-22 13:41:00 +0300

    robofriends first commit

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Card = () => {
+    return ( 
+        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+           <img src="https://robohash.org/web?200x200" alt="robots" />
+         <div>
+          <h2>Jonny Bravo</h2>
+          <p>jonnybravo@mail.com</p>
+         </div>
+        </div>
+        
+     );
+}
+ 
+export default Card ;
